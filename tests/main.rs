3e6fb85161316463ff95e737f9b2aeb6409fd420
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-01-27 11:39:05 +0300

    fix: Added mail to the licence

--- tests/main.rs
+++ tests/main.rs
@@ -1,5 +1,5 @@
 // Library for automatic generation of telegram bot api implementation for rust.
-// Copyright (C) 2025  Andrew Kozmin
+// Copyright (C) 2025  Andrew Kozmin <syn.kolbasyn.06@gmail.com>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
